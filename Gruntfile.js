eced788edf6a117cbe7bcdf692288f92883c2efb
Alexander Schenkel <aschenkel@kadenpartner.ch>
2020-05-23 10:04:55 +0200

    Umbau Infrastruktur, Jahresplan

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,6 @@
 /* global module:false */
 module.exports = function(grunt) {
 	var port = grunt.option('port') || 8000;
-    var sass = require('node-sass');
 	var root = grunt.option('root') || '.';
     var path = require('path');
     var nunjucks = require('nunjucks');
@@ -14,61 +13,6 @@ module.exports = function(grunt) {
 	// Project configuration
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		meta: {
-			banner:
-				'/*!\n' +
-				' * reveal.js <%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd, HH:MM") %>)\n' +
-				' * http://revealjs.com\n' +
-				' * MIT licensed\n' +
-				' *\n' +
-				' * Copyright (C) 2017 Hakim El Hattab, http://hakim.se\n' +
-				' */'
-		},
-
-		uglify: {
-			options: {
-				banner: '<%= meta.banner %>\n',
-				screwIE8: false
-			},
-			build: {
-				src: 'js/reveal.js',
-				dest: 'js/reveal.min.js'
-			}
-		},
-
-		sass: {
-            options: {
-                implementation: sass
-            },
-			core: {
-				src: 'css/reveal.scss',
-				dest: 'css/reveal.css'
-			},
-			themes: {
-				expand: true,
-				cwd: 'css/theme/source',
-				src: ['*.sass', '*.scss'],
-				dest: 'css/theme',
-				ext: '.css'
-			}
-		},
-
-		autoprefixer: {
-			core: {
-				src: 'css/reveal.css'
-			}
-		},
-
-		cssmin: {
-			options: {
-				compatibility: 'ie9'
-			},
-			compress: {
-				src: 'css/reveal.css',
-				dest: 'css/reveal.min.css'
-			}
-		},
-
 
 		connect: {
 			server: {
@@ -119,37 +63,18 @@ module.exports = function(grunt) {
 		},
 
 		watch: {
-			js: {
-				files: [ 'Gruntfile.js', 'js/reveal.js' ],
-				tasks: 'js'
-			},
-			theme: {
-				files: [
-					'css/theme/source/*.sass',
-					'css/theme/source/*.scss',
-					'css/theme/template/*.sass',
-					'css/theme/template/*.scss'
-				],
-				tasks: 'css-themes'
-			},
 			css: {
-				files: [ 'css/reveal.scss' ],
-				tasks: 'css-core'
+				files: root.map(path => path + '/**/*.css')
 			},
 			html: {
-				files: root.map(path => path + '/*.html')
+				files: root.map(path => path + '/**/*.html')
 			},
 			markdown: {
-				files: root.map(path => path + '/*.md')
+				files: root.map(path => path + '/**/*.md')
 			},
 			options: {
 				livereload: true
 			}
-		},
-
-		retire: {
-			js: [ 'js/reveal.js', 'lib/js/*.js', 'plugin/**/*.js' ],
-			node: [ '.' ]
 		},
 
         copy: {
@@ -161,7 +86,7 @@ module.exports = function(grunt) {
                         '!**/*.html',
                         '!**/*.scss',
                         '!css/theme/source/**',
-                        '!node_modules/**',
+                        '!node_modules*/**',
                         '!test/**',
                         '!CONTRIBUTING.md',
                         '!Gruntfile.js',
@@ -185,7 +110,8 @@ module.exports = function(grunt) {
                 files: [{
                 src: [
                     '**/*.html',
-                    '!node_modules/**',
+                    '!node_modules*/**',
+                    '!demo.html',
                     '!test/**',
                     '!plugin/**',
                     '!**/_*.html'
@@ -207,14 +133,9 @@ module.exports = function(grunt) {
 
 	// Dependencies
 	grunt.loadNpmTasks( 'grunt-contrib-connect' );
-	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
 	grunt.loadNpmTasks( 'grunt-contrib-copy' );
 	grunt.loadNpmTasks( 'grunt-contrib-clean' );
-	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
 	grunt.loadNpmTasks( 'grunt-contrib-watch' );
-	grunt.loadNpmTasks( 'grunt-autoprefixer' );
-	grunt.loadNpmTasks( 'grunt-retire' );
-	grunt.loadNpmTasks( 'grunt-sass' );
 	grunt.loadNpmTasks( 'grunt-zip' );
 
     grunt.registerMultiTask('nunjucks-render', 'generates html files from nunjucks templates', function(){
@@ -269,19 +190,7 @@ module.exports = function(grunt) {
     });
 
 	// Default task
-	grunt.registerTask( 'default', [ 'css', 'js' ] );
-
-	// JS task
-	grunt.registerTask( 'js', [ 'uglify' ] );
-
-	// Theme CSS
-	grunt.registerTask( 'css-themes', [ 'sass:themes' ] );
-
-	// Core framework CSS
-	grunt.registerTask( 'css-core', [ 'sass:core', 'autoprefixer', 'cssmin' ] );
-
-	// All CSS
-	grunt.registerTask( 'css', [ 'sass', 'autoprefixer', 'cssmin' ] );
+	grunt.registerTask( 'default', [ 'serve' ] );
 
 	// Package presentation to archive
 	grunt.registerTask( 'package', [ 'default', 'zip' ] );
@@ -290,6 +199,6 @@ module.exports = function(grunt) {
 	grunt.registerTask( 'serve', [ 'connect', 'watch' ] );
 
 	// Run build task: exports everything as standalone html to build/
-	grunt.registerTask( 'build', [ 'css', 'js', 'clean:build', 'nunjucks-render:build', 'copy:build' ] );
+	grunt.registerTask( 'build', [ 'clean:build', 'nunjucks-render:build', 'copy:build' ] );
 
 };
